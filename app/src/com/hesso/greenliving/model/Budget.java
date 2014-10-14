ba62fa9d57e12905b71adff7b64d2e4a21e6f74c
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2014-10-14 11:26:03 +0200

    UI dev

--- app/src/com/hesso/greenliving/model/Budget.java
+++ app/src/com/hesso/greenliving/model/Budget.java
@@ -8,4 +8,36 @@ public class Budget {
 	private int dayOfMonth;
 	private BigDecimal amount = new BigDecimal(0);
 	private Set<BudgetEntry> entries = new HashSet<BudgetEntry>();
+
+	public Set<BudgetEntry> getEntries() {
+		return new HashSet<BudgetEntry>(this.entries);
+	}
+
+	public void setEntries(Set<BudgetEntry> entries) {
+		this.entries = new HashSet<BudgetEntry>(entries);
+	}
+
+	public boolean addEntry(BudgetEntry entry) {
+		return this.entries.add(entry);
+	}
+
+	public boolean removeEntry(BudgetEntry entry) {
+		return this.entries.remove(entry);
+	}
+
+	public int getDayOfMonth() {
+		return dayOfMonth;
+	}
+
+	public void setDayOfMonth(int dayOfMonth) {
+		this.dayOfMonth = dayOfMonth;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
 }
---
+++ app/src/com/hesso/greenliving/ui/TransactionView.java
@@ -0,0 +1,56 @@
+package com.hesso.greenliving.ui;
+
+import java.text.DecimalFormat;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.hesso.greenliving.R;
+import com.hesso.greenliving.model.BudgetEntry;
+import com.hesso.greenliving.model.Transaction;
+
+public class TransactionView extends RelativeLayout {
+
+	private static final DecimalFormat DEC_FORMAT = new DecimalFormat("#0.00");
+
+	public static TransactionView inflate(ViewGroup parent) {
+		return (TransactionView) LayoutInflater.from(parent.getContext()).inflate(R.layout.item_transaction, parent, false);
+	}
+
+	private Transaction transaction;
+
+	private TextView from;
+	private TextView to;
+	private TextView amount;
+
+	public TransactionView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+
+		this.from = (TextView) this.findViewById(R.id.budgetFrom);
+		this.to = (TextView) this.findViewById(R.id.budgetTo);
+		this.amount = (TextView) this.findViewById(R.id.amount);
+	}
+
+	public void setItem(Transaction item) {
+		this.transaction = item;
+
+		this.amount.setText(DEC_FORMAT.format(this.transaction.getAmount()));
+		this.from.setText(this.transaction.getBudgetEntry().getName());
+
+		BudgetEntry to = this.transaction.getBudgetEntryDestination();
+		if (to != null) {
+			this.to.setVisibility(View.VISIBLE);
+			;
+			this.to.setText(to.getName());
+		}
+	}
+
+	public Transaction getItem() {
+		return this.transaction;
+	}
+}
